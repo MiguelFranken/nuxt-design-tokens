4fd86ca1a8c917270b1dd0886d9403f111ed0e76
miguelfranken <miguel.franken@rwth-aachen.de>
2022-12-21 19:26:34 +0100

    Initialize Nuxt 3

---
+++ app.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const counter = ref<number>(0);
+
+function increase() {
+  counter.value++;
+}
+
+function decrease() {
+  counter.value--;
+}
+</script>
+
+<template>
+  <div>
+    <span>Counter: {{ counter }}</span>
+    <button type="button" @click="increase">Increase</button>
+    <button type="button" @click="decrease">Decrease</button>
+  </div>
+</template>
