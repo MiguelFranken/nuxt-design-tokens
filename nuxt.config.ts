5edca4bffcfbc2ab04cb38241a17f886496bc423
miguelfranken <miguel.franken@rwth-aachen.de>
2022-12-21 20:26:01 +0100

    Load generated scss token variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/tokens/_variables.scss" as *;'
+        }
+      }
+    }
+  },
   modules: [
     '@nuxtjs/tailwindcss'
   ]
